df10d5442533e6f2add9130c471040e60afce536
shalanah <shalanahfaith@gmail.com>
2021-03-12 19:40:28 -0600

    Simplify more

--- src/index.js
+++ src/index.js
@@ -9,33 +9,13 @@ const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 const App = () => {
   return (
-    <Elements stripe={stripePromise}>
-      <div
-        style={{
-          margin: "0 auto",
-          maxWidth: "500px",
-          padding: "0 24px",
-          display: "flex",
-          flexDirection: "column",
-          height: "100vh",
-        }}
-      >
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            paddingBottom: "40%",
-          }}
-        >
-          <CardForm />
-        </div>
-      </div>
-    </Elements>
+    <div style={{ margin: "auto" }}>
+      <Elements stripe={stripePromise}>
+        <CardForm />
+      </Elements>
+    </div>
   );
 };
 
 const rootElement = document.getElementById("root");
-
 ReactDOM.render(<App />, rootElement);
